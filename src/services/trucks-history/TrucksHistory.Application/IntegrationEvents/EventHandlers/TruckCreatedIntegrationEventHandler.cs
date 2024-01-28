70937c80b410312bad570021de7d2717dc229e95
Jakub Gołębiowski <j.g.golebiowski@gmail.com>
2024-01-28 22:46:44 +0100

    api consuming messages added

---
+++ src/services/trucks-history/TrucksHistory.Application/IntegrationEvents/EventHandlers/TruckCreatedIntegrationEventHandler.cs
@@ -0,0 +1,21 @@
+﻿using EventBus.Abstractions;
+using Microsoft.Extensions.Logging;
+using TrucksHistory.Application.IntegrationEvents.Events;
+
+namespace TrucksHistory.Application.IntegrationEvents.EventHandlers;
+public class TruckCreatedIntegrationEventHandler : IIntegrationEventHandler<TruckCreatedIntegrationEvent>
+{
+    private readonly ILogger<TruckCreatedIntegrationEventHandler> _logger;
+
+    public TruckCreatedIntegrationEventHandler(ILogger<TruckCreatedIntegrationEventHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Handle(TruckCreatedIntegrationEvent @event)
+    {
+        _logger.LogInformation("TRUCK-HISTORY: HANDLING INTEGRATION EVENT (EVENT_ID: {0}, TRUCK ID: {1}", @event.Id, @event.TruckId);
+
+        return Task.CompletedTask;
+    }
+}
---
+++ src/services/trucks-history/trucks-history.api/Extensions/EventBusConfigurationExtensions.cs
@@ -0,0 +1,14 @@
+﻿using EventBus.Abstractions;
+using TrucksHistory.Application.IntegrationEvents.EventHandlers;
+using TrucksHistory.Application.IntegrationEvents.Events;
+
+namespace trucks_history.api.Extensions;
+
+public static class EventBusConfigurationExtensions
+{
+    public static void ConfigureEventBus(this IApplicationBuilder app)
+    {
+        var eventBus = app.ApplicationServices.GetRequiredService<IEventBus>();
+        eventBus.Subscribe<TruckCreatedIntegrationEvent, TruckCreatedIntegrationEventHandler>();
+    }
+}
